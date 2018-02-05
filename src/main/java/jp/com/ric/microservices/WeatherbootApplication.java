a32ff2baaff28de09e16454d6f7a6f518a85007b
esakat <esaka.tom@gmail.com>
2018-02-05 22:40:51 +0900

    first commit

---
+++ src/main/java/jp/com/ric/microservices/WeatherbootApplication.java
@@ -0,0 +1,12 @@
+package jp.com.ric.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherbootApplication.class, args);
+	}
+}
